eaed47628d86dff22ce2f3070b7ba579dec9aeab
Guillaume Audinet <guillaume.audinet@epita.fr>
2015-04-06 05:04:12 +0200

    greyscale

--- sources/filtres.c
+++ sources/filtres.c
@@ -1,6 +1,6 @@
 #include "../includes/interface.h"
 
-SDL_Surface* filtre_noir_blanc(t_display display)
+t_display* filtre_noir_blanc(t_display *display)
 {
 	
 		int x, y;
@@ -9,40 +9,90 @@ SDL_Surface* filtre_noir_blanc(t_display display)
  
         Uint32 pixel_s;
  
-        SDL_LockSurface(display.screen);
+        SDL_LockSurface(display->screen);
  
-        for (y = 0 ; y < display.screen->h; y ++)
+        for (y = 0 ; y < display->screen->h; y ++)
         {
-                for ( x = 0 ; x < display.screen->w; x ++)
+                for ( x = 0 ; x < display->screen->w; x ++)
                 {
-                        pixel_s = *((Uint32*)(display.screen->pixels) + x + y * WINX);
+                        pixel_s = *((Uint32*)(display->screen->pixels) + x + y * WINX);
  
-                        SDL_GetRGBA(pixel_s, display.screen->format, &r_s, &g_s, &b_s, &a_s);
+                        SDL_GetRGBA(pixel_s, display->screen->format, &r_s, &g_s, &b_s, &a_s);
  
                         Uint8 Moyenne = (r_s + g_s + b_s) / 3;
  
-                        if(Moyenne < 123)
+                        if(Moyenne < 128)
                         {
                             r_s = 0;
                             g_s = 0;
                             b_s = 0;
                         }
-                        else if(Moyenne >= 124)
+                        else if(Moyenne >= 128)
                         {
                             r_s = 255;
                             g_s = 255;
                             b_s = 255;
                         }
  
-                        pixel_s = SDL_MapRGBA(display.screen->format, r_s, g_s, b_s, a_s);
+                        pixel_s = SDL_MapRGBA(display->screen->format, r_s, g_s, b_s, a_s);
  
-                        setPixel(&display, x, y, pixel_s);
+                        setPixel(display, x, y, pixel_s);
  
                 }
         }
 
-        SDL_UnlockSurface(display.screen);
+        SDL_UnlockSurface(display->screen);
+		return (display);
 
-	return display.screen;
+}
 
+t_display* filtre_gris(t_display *display)
+{
+	int x, y;
+ 
+    Uint8 r_s, g_s, b_s, a_s;
+ 
+    Uint32 pixel_s;
+ 
+    SDL_LockSurface(display->screen);
+
+	
+	//TODO
+	for (y = 0 ; y < display->screen->h; y ++)
+        {
+                for ( x = 0 ; x < display->screen->w; x ++)
+                {
+					//
+					pixel_s = *((Uint32*)(display->screen->pixels) + x + y * WINX);
+ 
+                    SDL_GetRGBA(pixel_s, display->screen->format, &r_s, &g_s, &b_s, &a_s);
+ 
+                    Uint8 Moyenne = (r_s * 0.3 + g_s * 0.59 + b_s * 0.11);
+
+					if(Moyenne < 128)
+                        {
+                            r_s = 0;
+                            g_s = 0;
+                            b_s = 0;
+                        }
+                     else if(Moyenne >= 128)
+                        {
+                            r_s = 255;
+                            g_s = 255;
+                            b_s = 255;
+                        }
+ 
+                        pixel_s = SDL_MapRGBA(display->screen->format, r_s, g_s, b_s, a_s);
+						setPixel(display, x, y, pixel_s);
+
+				}
+
+		}
+
+
+
+
+  	SDL_UnlockSurface(display->screen);
+
+	return (display);
 }

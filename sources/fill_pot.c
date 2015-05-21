694a04e076c47822f0bb6d1e8ff9fb21c30bf841
Thomas Nguyen <nguye_9@epita.fr>
2015-05-22 00:19:52 +0200

    postfix

--- sources/fill_pot.c
+++ sources/fill_pot.c
@@ -3,45 +3,41 @@
 #include "../includes/display.h"
 #include "../includes/couleurs.h"
 
-
-Uint32 ColorPick(t_display *display, int x, int y)
-{
-	Uint8 r_s, g_s, b_s, a_s;
-	Uint32 pixel_s;
-	pixel_s = *((Uint32*)(display->screen->pixels) + x + y * WINX);
-	SDL_GetRGBA(pixel_s, display->screen->format, &r_s, &g_s, &b_s, &a_s);
-	pixel_s = SDL_MapRGBA(display->screen->format, r_s, g_s, b_s, a_s);
-
-	return pixel_s;
-}
-
-void rec_pot(t_display *display, Uint32 pixel_s, int x, int y, int M[])
-{
-	M[x + y * WINX] = 0;
 	Uint8 r_s, g_s, b_s, a_s;
 	Uint8 r, g, b, a;
 	Uint32 pixel;
+	t_display *displayer;
+	int M[WINX*WINY];
 
-SDL_GetRGBA(pixel_s, display->screen->format, &r_s, &g_s, &b_s, &a_s);
-	
-	pixel = *((Uint32*)(display->screen->pixels) + x + y * WINX);
-	SDL_GetRGBA(pixel, display->screen->format, &r, &g, &b, &a);
+void ColorPick(int x, int y)
+{
+	pixel = *((Uint32*)(displayer->screen->pixels) + x + y * WINX);
+	SDL_GetRGBA(pixel, displayer->screen->format, &r_s, &g_s, &b_s, &a_s);
+	M[x + y * WINX] = 1;
+}
 
-	if((r_s == r)&&(g_s == g)&&(b_s == b))
+void rec_pot(int x, int y)
+{
+	//M[x + y * WINX] = 0;
+	pixel = *((Uint32*)(displayer->screen->pixels) + x + y * WINX);
+	SDL_GetRGBA(pixel, displayer->screen->format, &r, &g, &b, &a);
+
+	if(r_s == r)
 	{
-		setPixelVerif(display, x, y, colors[display->color_index]);
-			if(y-1>=0){
-			if(M[x + (y - 1) * WINX])
-				rec_pot(display, pixel_s, x, y - 1, M);}
+		setPixel(displayer, x, y, colors[displayer->color_index]);
+		M[x + y * WINX] = 1;
+		if(y-1>=0){
+			if(!M[x + (y - 1) * WINX])
+				rec_pot(x, y - 1);}
 			if(y+1<WINY){	
-			if(M[x + (y + 1) * WINX])
-				rec_pot(display, pixel_s, x, y + 1, M);}
+			if(!M[x + (y + 1) * WINX])
+				rec_pot(x, y + 1);}
 			if(x+1<WINX){
-			if(M[x + 1 + y * WINX])
-				rec_pot(display, pixel_s, x + 1, y, M);}
+			if(!M[x + 1 + y * WINX])
+				rec_pot(x + 1, y);}
 			if(x-1>=0){
-			if(M[x - 1 + y * WINX])
-				rec_pot(display, pixel_s, x - 1, y, M);}
+			if(!M[x - 1 + y * WINX])
+				rec_pot(x - 1, y);}
 
 	}
 
@@ -51,23 +47,31 @@ SDL_GetRGBA(pixel_s, display->screen->format, &r_s, &g_s, &b_s, &a_s);
 int fill_pot(t_display *display, void *param)
 {
 	SDL_MouseButtonEvent	*mouse;
-
+	displayer = display;
 	if(display->button == RELEASED)
 		return (0);
-	Uint32 pixel_s;
 	int x, y, n;
 	mouse = (SDL_MouseButtonEvent *)param;
 	x = mouse->x;
 	y = mouse->y;
-	pixel_s = ColorPick(display, x, y);
+	ColorPick(x, y);
 	
 	n = WINX * WINY;
-	int M[n];
-	for(int i = 0; i < n; i++)
+	/*for(int i = 0; i < n; i++)
 	{
-		M[i] = 1;
+		M[i] = 0;
 	}
-	rec_pot(display, pixel_s, x, y, M);
+	rec_pot(x, y);*/
+	int i = 0;
+	while(i < n)
+	{
+	pixel = *((Uint32*)(displayer->screen->pixels) + i);
+	SDL_GetRGBA(pixel, displayer->screen->format, &r, &g, &b, &a);
+
+	if((r_s == r)&&(g_s == g)&&(b_s == b))
+		*((Uint32*)(display->screen->pixels) + i) = colors[display->color_index];
+	i++;
+	}
 	
 	return SDL_Flip(display->screen);
 }

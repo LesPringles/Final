3a2b12004d8c509640200f8809d36b002b201fba
ballot_g <ballot_g@epitech.eu>
2015-05-21 22:49:32 +0200

    Layers undo redo for pixel tracing

--- sources/print_pixel.c
+++ sources/print_pixel.c
@@ -9,13 +9,14 @@ void disc(t_display *display, int cx, int cy, long rayon, Uint32 coul);
 
 int			print_pixel_carre(t_display *display, void *param)
 {
+  static t_button_state	last_state = UNSET;
   SDL_MouseButtonEvent	*mouse;
   SDL_Surface		*pixel;
   SDL_Rect		pos;
+  SDL_Rect		pos_0;
 
-
-
-  if (display->button == RELEASED)
+  memset(&pos_0, 0, sizeof(SDL_Rect));
+  if (display->button == RELEASED && last_state == UNSET)
     return (0);
   mouse = (SDL_MouseButtonEvent *)param;
   pos.x = mouse->x - 2;
@@ -26,23 +27,30 @@ int			print_pixel_carre(t_display *display, void *param)
     return -1;
   if (SDL_BlitSurface(pixel, NULL, display->screen, &pos) == -1)
     return -1;
-  
-  /*if (add_layer(&display->layers, display->screen, &pos_0) == -1)
-	return -1;*/
 
+  if (((last_state == UNSET) || (last_state == RELEASED))
+      && (display->button == PRESSED))
+    last_state = PRESSED;
 
+  if (display->button == RELEASED)
+    {
+      if (add_layer(&display->layers, display->screen, &pos_0) == -1)
+	return -1;
+      last_state = UNSET;
+    }
   return SDL_Flip(display->screen);
 }
 
 int			print_pixel_rond(t_display *display, void *param)
 {
+  static t_button_state	last_state = UNSET;
   SDL_MouseButtonEvent	*mouse;
   SDL_Surface		*pixel;
   SDL_Rect		pos;
+  SDL_Rect		pos_0;
 
-
-
-  if (display->button == RELEASED)
+  memset(&pos_0, 0, sizeof(SDL_Rect));
+  if (display->button == RELEASED && last_state == UNSET)
     return (0);
   mouse = (SDL_MouseButtonEvent *)param;
   pos.x = mouse->x - 2;
@@ -50,14 +58,21 @@ int			print_pixel_rond(t_display *display, void *param)
   if ((pixel = SDL_CreateRGBSurface(0, 0, 0, 32, 0, 0, 0, 0)) == NULL)
     return -1;
 
+  if (((last_state == UNSET) || (last_state == RELEASED))
+      && (display->button == PRESSED))
+    last_state = PRESSED;
+
   disc(display, pos.x, pos.y, 3, colors[display->color_index]);
+
+  if (display->button == RELEASED)
+    {
+      if (add_layer(&display->layers, display->screen, &pos_0) == -1)
+	return -1;
+      last_state = UNSET;
+    }
 
   if (SDL_BlitSurface(pixel, NULL, display->screen, &pos) == -1)
     return -1;
-  
-  /*if (add_layer(&display->layers, display->screen, &pos_0) == -1)
-	return -1;*/
-
 
   return SDL_Flip(display->screen);
 }
